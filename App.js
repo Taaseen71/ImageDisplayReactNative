549a58e994921f7f6e7db5a5788876239ec65ecd
Saadat Taaseen <saadat.taaseen@gmail.com>
2020-12-11 03:34:15 -0500

    added a url input that creates new object

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button, Image, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, FlatList, TextInput } from 'react-native';
 import images from './Components/imageLists.json'
+import { v4 as uuidV4 } from 'uuid'
+// import { save } from 'save-file'
+var fs = require('fs');
+
+
 
 export default function App() {
 
     const [imageList, setImageList] = useState([{}])
+    const [urlInput, seturlInput] = useState("")
+    const [urlDescription, seturlDescription] = useState("")
+
+
+
 
     useEffect(() => {
         setImageList(images);
@@ -13,15 +23,31 @@ export default function App() {
     }, [])
 
 
+    const handleAdd = () => {
+        const newImage = urlInput
+        const description = urlDescription
+        setImageList([...imageList, { id: uuidV4(), photo: newImage, description: description }])
+        seturlInput("");
+        seturlDescription("");
+        let images = JSON.stringify(imageList);
+        // alert(images);  1
+
+    }
+
+
     return (
         <View style={styles.container}>
             <Text> ImageUploader </Text>
+            <TextInput placeholder="Enter New Image URL" value={urlInput} onChangeText={e => seturlInput(e)} />
+            <TextInput placeholder="Description for this Photo" value={urlDescription} onChangeText={e => seturlDescription(e)} />
+            <Button title="Add" onPress={handleAdd} />
             <FlatList data={imageList} renderItem={itemData => (
                 <View style={styles.imageContainer} >
                     {/* <Text> {itemData.item.id}</Text> */}
                     <Image source={{ uri: itemData.item.photo }} style={styles.ImageList} resizeMode="contain" />
                     <View style={styles.textDescription}>
                         <Text >{itemData.item.description}</Text>
+                        <Text> {itemData.item.id}</Text>
                     </View>
                 </View>
             )} />
